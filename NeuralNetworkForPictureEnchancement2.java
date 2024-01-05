13ed2bb05e6208d94dd015e022f9e096c59d0083
Timothy Kimball <82687508+TimothyKimball-design@users.noreply.github.com>
2024-01-05 01:22:48 -0500

    Update NeuralNetworkForPictureEnchancement2.java

--- NeuralNetworkForPictureEnchancement2.java
+++ NeuralNetworkForPictureEnchancement2.java
@@ -1,7 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Made by Tim 
+
 package com.mycompany.computer.vision;
 
 import java.awt.Color;
